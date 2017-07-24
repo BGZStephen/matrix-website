de949d6f53c904bbda7db74c1400403790cffd0a
Stephen Wright <sjw948@gmail.com>
2017-07-24 07:54:44 +0100

    added toggle nav for smaller screens to gently slide out nav on toggle

--- src/app/modules/dashboard/components/dashboard-navbar/dashboard-navbar.component.ts
+++ src/app/modules/dashboard/components/dashboard-navbar/dashboard-navbar.component.ts
@@ -9,6 +9,21 @@ export class DashboardNavbarComponent implements OnInit {
 
   constructor() { }
 
+  navbarVisible = false;
+
+  navbarStyle() {
+    if(!this.navbarVisible) {
+      return {'min-height': '35px', 'max-height': '35px'}
+    } else {
+      const height = document.getElementsByClassName('top-level-nav')[0].children.length * 35 + 'px'
+      return {'min-height': height, 'max-height': 'auto'}
+    }
+  }
+
+  toggleNavbarVisible() {
+    this.navbarVisible = !this.navbarVisible
+  }
+
   ngOnInit() {
   }
 
